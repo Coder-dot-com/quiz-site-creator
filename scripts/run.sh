14d5ac2106ea6b237cefece4b787411f7e5a406b
Your Name <usman17.s@hotmail.com>
2023-04-15 21:49:16 +0100

    fix update collectstatic

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ set -e
 whoami
 
 python manage.py wait_for_db
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 
 python manage.py migrate
 python manage.py fixtree
